ea2f8694d3f93064d48f243023d8e9f4ecc7b805
Sean <shonum@gmail.com>
2016-10-18 12:29:36 -0700

    Added unit tests for Game constructor

--- app/game.js
+++ app/game.js
@@ -2,7 +2,22 @@ class Player {
   constructor(name) {
     this.name = name;
   }
-  move() {
+  speak() {
     return this.name + ' is moving';
+  }
+}
+
+
+class Game {
+  constructor(rounds, player1, player2) {
+    this.board = [[null, null, null], [null, null, null], [null, null, null]];
+    this.rounds = rounds;
+    this.player1 = player1;
+    this.player2 = player2;
+    this.currentRound = 1;
+  }
+
+  turn(player) {
+
   }
 }
--- specs/Spec.js
+++ specs/Spec.js
@@ -3,16 +3,27 @@ const expect = chai.expect;
 describe("Player", function() {
   describe("constructor", function() {
     it("should have a default name", function() {
-      var player = new Player('Sean');
+      let player = new Player('Sean');
       expect(player.name).to.equal('Sean');
-    });
-
-  });
-
-  describe("#move", function() {
-    it("should should return a string", function() {
-      var move = (new Player('Andrew')).move();
-      expect(move).to.equal('Andrew is moving');
     });
   });
 });
+
+describe("Game", function() {
+  describe("constructor", function() {
+    let player1 = new Player('Sean');
+    let player2 = new Player('Kunal');
+
+    let game = new Game(3, player1, player2);
+
+    it("should have a number of rounds", function() {
+      expect(game.rounds).to.equal(3);
+    });
+
+    it("should start with a board", function() {
+      expect(game.board.length).to.equal(3);
+      expect(game.board[0].length).to.equal(3);
+    });
+  });
+});
+
